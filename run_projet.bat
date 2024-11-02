b44970037c7b38473d7b5700f3036d2546f8cd06
chris-sem <semjeanchristopher4@gmail.com>
2024-11-02 19:15:18 +0100

    Ajout bat pour lancement automatique

---
+++ run_projet.bat
@@ -0,0 +1,6 @@
+@echo off
+rem Nettoie et compile le projet avec Maven
+rem mvn clean package
+
+rem Exécute le programme avec le module JavaFX
+java --module-path .\javafx-sdk-21.0.4\lib\ --add-modules javafx.controls,javafx.fxml -jar .\target\session_phase1-1.0-SNAPSHOT.jar
