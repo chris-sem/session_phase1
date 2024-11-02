bf2ba3b72e957abe840adb0094f2a241ce0e80fd
Myriam <myriam.hj.2002@gmail.com>
2024-11-02 11:44:39 +0100

    Heyyyy

--- src/main/java/isty/iatic5/session_phase1/HelloApplication.java
+++ src/main/java/isty/iatic5/session_phase1/HelloApplication.java
@@ -11,6 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/View/accueil-view.fxml"));
+        //Taille de la fenêtre
         Scene scene = new Scene(fxmlLoader.load(), 1000, 750);
         stage.setTitle("Accueil");
         stage.setScene(scene);
