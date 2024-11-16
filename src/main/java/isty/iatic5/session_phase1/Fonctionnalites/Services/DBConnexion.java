b47e1fa832fdf3698f327122f9e95dbcc0c44096
chris-sem <semjeanchristopher4@gmail.com>
2024-11-16 18:48:00 +0100

    Finale finale

--- src/main/java/isty/iatic5/session_phase1/Fonctionnalites/Services/DBConnexion.java
+++ src/main/java/isty/iatic5/session_phase1/Fonctionnalites/Services/DBConnexion.java
@@ -1,6 +1,10 @@
 package isty.iatic5.session_phase1.Fonctionnalites.Services;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DBConnexion {
     //Pour la connexion
@@ -12,21 +16,28 @@ public class DBConnexion {
     //Pour les mise à jour : Insert into, update, delete
     private int ok;
 
+    private static class DBConfig {
+        public String host;
+        public String port;
+        public String db;
+        public String user;
+        public String password;
+    }
+
     public Connection getConnection(){
-        String host = "localhost";
-        String port = "3306";
-        String db = "sdial";
-        String url = "jdbc:mysql://" + host + ":" + port + "/" + db;
-        String user = "groupe3";
-        String password = "mdp-groupe3";
 
-        try{
-            //Chargement du pilote
-            //Class.forName("com.mysql.jdbc.Driver");
-            //On essaie d'ouvrir la connexion
-            cnx = DriverManager.getConnection(url, user, password);
+        try {
+
+            String configFilePath = "config_BD.txt";
+
+            Map<String, String> config = readConfigFromTxt(configFilePath);
+
+            // Construire l'URL de connexion
+            String url = "jdbc:mysql://" + config.get("host") + ":" + config.get("port") + "/" + config.get("db");
+
+            // Charger la connexion
+            cnx = DriverManager.getConnection(url, config.get("user"), config.get("password"));
             //System.out.println("Connected to the database successfully");
-
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -78,6 +89,24 @@ public class DBConnexion {
 
     public PreparedStatement getPstm(){
         return pstm;
+    }
+
+
+    private Map<String, String> readConfigFromTxt(String filePath) throws Exception {
+        Map<String, String> config = new HashMap<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Ignorer les lignes vides ou incorrectes
+                if (line.contains(":")) {
+                    String[] keyValue = line.split(":", 2);
+                    config.put(keyValue[0].trim(), keyValue[1].trim());
+                }
+            }
+        }
+
+        return config;
     }
 
 }
